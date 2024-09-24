9cf7259a9d29380e1ad17179afb0571db81bcece
atowey_uchi <atowey@uchicago.edu>
2024-09-24 10:15:55 -0400

    added additonal wait times and example csv ouput

--- naomi_api_plumber.R
+++ naomi_api_plumber.R
@@ -18,18 +18,18 @@ initialize_driver <- function() {
   # Set Chrome options
   chrome_options <- list(
     chromeOptions = list(
-      args = c('--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage')
+      args = c('--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage')
     )
   )
-  
+
   # Initialize RSelenium driver with Chrome options
   rD <- rsDriver(browser = "chrome", chromever = "latest", extraCapabilities = chrome_options)
   remDr <- rD$client
-  
+
   return(list(remDr = remDr, rD = rD))
 }
 
-click_element <- function(remDr, xpath, retries = 3, delay = 1) {
+click_element <- function(remDr, xpath, retries = 10, delay = 1) {
   for (attempt in 1:retries) {
     tryCatch({
       message(sprintf("Attempting to click element with XPath: %s", xpath))
@@ -80,18 +80,18 @@ parse_table <- function(remDr) {
   message("Parsing table.")
   table_element <- remDr$findElement(using = 'xpath', '//table')
   page_source <- remDr$getPageSource()[[1]]
-  
+
   # Use rvest to parse HTML table
   page <- read_html(page_source)
   table <- page %>%
     html_table(fill = TRUE) %>%
     .[[1]]
-  
+
   # Print table name for verification
   table_name_xpath <- '//*[@class="MuiTypography-root MuiTypography-caption css-1v8m1vc"]'
   table_name <- remDr$findElement(using = 'xpath', table_name_xpath)
   message(sprintf("Table Name: %s", table_name$getElementText()[[1]]))
-  
+
   return(table)
 }
 
@@ -106,59 +106,66 @@ parse_table <- function(remDr) {
 function(country, indicator, age_group, period, sex, csv = FALSE) {
   driver_data <- initialize_driver()
   remDr <- driver_data$remDr
-  
+
   tryCatch({
     message("Navigating to the website.")
     remDr$navigate("https://naomi-spectrum.unaids.org/")
-    
+
     # Handle potential overlay
     #handle_overlay(remDr)
-    
+
     # Select Country
     message("Selecting country.")
     click_element(remDr, '//button[contains(text(), "Select countries")]')
     Sys.sleep(1)  # Ensure dropdown is loaded
-    
+
     country_option_xpath <- sprintf('//span[contains(text(), "%s")]', country)
     click_element(remDr, country_option_xpath)
     click_element(remDr, '//button[contains(text(), "Apply")]')
-    
+    Sys.sleep(1)
+
     # Select Indicator
     message("Selecting indicator.")
     select_option(remDr, '//div[@aria-controls=":r6:"]', sprintf('//li[contains(text(), "%s")]', indicator))
-    
+    Sys.sleep(1)
+
     # Select Age Group
     message("Selecting age group.")
     select_option(remDr, '//div[@aria-controls=":r7:"]', sprintf('//li[contains(text(), "%s")]', age_group))
-    
+    Sys.sleep(1)
+
     # Select Period
     message("Selecting period.")
     select_option(remDr, '//div[@aria-controls=":r8:"]', sprintf('//li[contains(text(), "%s")]', period))
-    
+    Sys.sleep(1)
+
     # Select Sex
     message("Selecting sex.")
     select_option(remDr, '//div[@aria-controls=":r9:"]', sprintf('//li[contains(text(), "%s")]', sex))
-    
+    Sys.sleep(1)
+
     # Select Area Level (Last in List is Most Specific)
     message("Selecting area level.")
     click_element(remDr, '//div[@aria-controls=":ra:"]')
     area_level_option_xpath <- '(//li[contains(@role, "option")])[last()]'
     select_option(remDr, '//div[@aria-controls=":ra:"]', area_level_option_xpath)
-    
+    Sys.sleep(1)
+
     # Toggle to Table View
     message("Toggling to table view.")
     toggle_switch_xpath <- '//span[contains(@class, "MuiSwitch-root")]'
     click_element(remDr, toggle_switch_xpath)
-    
+    Sys.sleep(1)
+
     # Optionally download CSV
     if (csv) {
       download_csv(remDr)
     }
-    
+
     # Parse and return table
     df <- parse_table(remDr)
     return(df)
-    
+
   }, finally = {
     remDr$close()
     driver_data$rD$server$stop()
